e356b45380f32b4f26f257b7abe3afe6d2a2f206
AI Professionals <aiprofessionals061@gmail.com>
2024-07-22 10:19:03 +0500

    db config updated

--- database/db.config.js
+++ database/db.config.js
@@ -4,7 +4,6 @@ module.exports = {
   USER: process.env.DB_USER,
   PASSWORD: process.env.DB_PASSWORD,
   DB: process.env.DB_DBNAME,
-  dialect: process.env.DB_DIALECT,
-  port: process.env.DB_PORT
-
+  DIALECT: process.env.DB_DIALECT,
+  PORT: process.env.DB_PORT
 };
